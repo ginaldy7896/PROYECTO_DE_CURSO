15cdad5ba6c766c6212d66f11aeec5503703657d
ginaldy7896 <ginaldy.ramirez@uao.edu.co>
2026-05-29 03:18:25 -0500

    cambios escena 2

--- ATHELGARD/Assets/GAME/Scripts/Escena1/Scene1Manager.cs
+++ ATHELGARD/Assets/GAME/Scripts/Escena1/Scene1Manager.cs
@@ -39,14 +39,15 @@ public class Scene1Manager : MonoBehaviour
         }
     }
 
-    
+
 
     void Update()
     {
         bool presionoSalto = Input.GetButtonDown("Jump");
         // Si ya detectamos el movimiento una vez, ignoramos el resto del Update
-        if (instruccionesCompletadas) { 
-        
+        if (instruccionesCompletadas)
+        {
+
             textoCartel.text = "Preciona espacio para saltar, y el mouse para mirar alrededor. ¡Explora el entorno!";
 
             if (presionoSalto)
@@ -120,7 +121,7 @@ public class Scene1Manager : MonoBehaviour
                 yield return null;
             }
 
-            
+
             contenedorCartelUI.gameObject.SetActive(false);
         }
     }
